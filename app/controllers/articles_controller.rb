65385f7802a6a67386b60f52fedb56766f937723
Mike Mann <miketheman@t-online.de>
2017-06-04 15:55:58 +0000

    Add partials and destroy action"

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,14 +13,13 @@ class ArticlesController < ApplicationController
   end
  
   def create
-   @article = Article.new(article_params)
-   if @article.save
-     flash[:notice] = "Article was successfully created"
-     redirect_to article_path(@article)
-   else
-     render 'new'
-   end
-   
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Article was successfully created"
+      redirect_to article_path(@article)
+    else
+       render 'new'
+    end
   end
   
   def update
@@ -37,6 +36,12 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article was successfully deleted"
+    redirect_to articles_path
+  end  
 
 
   private
