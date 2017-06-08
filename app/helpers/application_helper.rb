44ed720d7a5dda9bcf580dabf065ca528fb68740
Mike Mann <miketheman@t-online.de>
2017-06-08 22:55:55 +0000

    Add users listing and update show article view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,3 +6,6 @@ module ApplicationHelper
        image_tag(gravatar_url, alt: user.username, class: "img-circle")
     end
 end
+
+# https://s.gravatar.com/avatar/b416039c480a28c2749eb8fb48c5e7b7?s=80
+#   gravatar_url = "https://s.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
