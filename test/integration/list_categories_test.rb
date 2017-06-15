28ccccdc4e344cfbaf9fd4fe030d5f1b36abeec0
Mike Mann <miketheman@t-online.de>
2017-06-15 00:14:01 +0000

    Fix category list bug

--- test/integration/list_categories_test.rb
+++ test/integration/list_categories_test.rb
@@ -1,16 +1,16 @@
 require 'test_helper'
 
-class CreateCategoriesTest < ActionDispatch::IntegrationTest
+class ListCategoriesTest < ActionDispatch::IntegrationTest
   
  def setup
-   @category_book = Category.create(name: "books")
+   @category_sports = Category.create(name: "sports")
    @category_programming = Category.create(name: "programming")
  end
  
  test "should show categories listing" do
    get categories_path
    assert_template 'categories/index'
-   assert_select "a[href=?]", category_path(@category_book), text: @category_book.name
+   assert_select "a[href=?]", category_path(@category_sports), text: @category_sports.name
    assert_select "a[href=?]", category_path(@category_programming), text: @category_programming.name
  end
    
