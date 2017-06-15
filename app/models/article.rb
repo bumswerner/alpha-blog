0f625bd9a58488b2d3da1d9668f7973718c06364
Mike Mann <miketheman@t-online.de>
2017-06-15 12:48:56 +0000

    Add article and category association

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,10 +1,15 @@
 class Article < ActiveRecord::Base
   
+  # associations to user
   belongs_to :user
   
+  # associations to article_categories and categories
+  has_many :article_categories
+  has_many :categories, through: :article_categories
+  
+  # database contains
   validates :title, presence: true, length: {minimum: 3, maximum: 50 }
   validates :description, presence: true, length: {minimum: 5, maximum: 500 }
-  
   validates :user_id, presence: true
   
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,11 @@
 class Category < ActiveRecord::Base
+  
+  # association to article_categories and articles
+  has_many :article_categories
+  has_many :articles, through: :article_categories
+  
+  # database constains
   validates :name, presence: true, length: { minimum: 3, maximum: 25 }
   validates_uniqueness_of :name
+  
 end
